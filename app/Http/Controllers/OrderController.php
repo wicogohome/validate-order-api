efb637f90942f12af1d3f5e42939ea1333732819
Wico Tang <wicotang@gmail.com>
2024-09-21 20:48:01 +0800

    feat: add /api/orders entry

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class OrderController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+
+    public function validate()
+    {
+    }
+}
