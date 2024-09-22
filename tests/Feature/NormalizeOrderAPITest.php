d49d71587deb11d6db6934d7889016704372ddcd
Wico Tang <wicotang@gmail.com>
2024-09-22 17:56:19 +0800

    test: Feature: NormalizeOrderAPITest (simple)

---
+++ tests/Feature/NormalizeOrderAPITest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class NormalizeOrderAPITest extends TestCase
+{
+    public function testSuccessfulResponse(): void
+    {
+        $validInput = [
+            'id' => 'A0000001',
+            'name' => 'Melody Holidy Inn',
+            'address' => [
+                'city' => 'taipei-city',
+                'district' => 'da-an-district',
+                'street' => 'fuxing-south-road',
+            ],
+            'price' => 2000,
+            'currency' => 'TWD',
+        ];
+        $response = $this->postJson(route('api.order.normalize'), $validInput);
+        $response->assertStatus(200);
+    }
+
+    public function testInvalidInputResponse(): void
+    {
+        $invalidInput = [];
+        $response = $this->postJson(route('api.order.normalize'), $invalidInput);
+        $response->assertStatus(422);
+    }
+
+    public function testExampleResponse(): void
+    {
+        $exampleInput = [
+            'id' => 'A0000001',
+            'name' => 'Melody Holidy Inn',
+            'address' => [
+                'city' => 'taipei-city',
+                'district' => 'da-an-district',
+                'street' => 'fuxing-south-road',
+            ],
+            'price' => 2500,
+            'currency' => 'TWD',
+        ];
+
+        $response = $this->postJson(route('api.order.normalize'), $exampleInput);
+        $response->assertStatus(400);
+    }
+
+}
