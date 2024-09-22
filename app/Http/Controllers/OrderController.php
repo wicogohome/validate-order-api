2e7e5834682bf2a7946e21a01e3cf32cfd3ae729
Wico Tang <wicotang@gmail.com>
2024-09-22 15:11:04 +0800

    feat: OrderController@normalize: api function & add route name prefix

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\OrderService;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\NormalizeOrderRequest;
 
 class OrderController extends Controller
 {
-    public function __construct()
-    {
-    }
+    public function __construct(public OrderService $OrderService) {}
 
-
-    public function validate()
+    /**
+     * 透過驗證與轉換，將Order data統一為指定格式
+     *
+     * @param NormalizeOrderRequest
+     *
+     * @throws \Throwable
+     */
+    public function normalize(NormalizeOrderRequest $request): JsonResponse
     {
+        $data = $request->validated();
+
+        try {
+            $transformedResult = $this->OrderService->validateAndTransform($data);
+        } catch (\Throwable $e) {
+            throw $e;
+        }
+
+        return response()->json([
+            'status' => 200,
+            'data' => $transformedResult,
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
+Route::name('api.')->group(function () {
     Route::post('/orders', [OrderController::class, 'normalize'])->name('order.normalize');
+});
